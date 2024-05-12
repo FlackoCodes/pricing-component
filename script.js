7786499c5304fd3edf4ab98d40e9dae2d85bea80
Flacko <lesliejoe2k2@gmail.com>
2024-05-12 10:28:10 +0000

    new feat: toggle to change

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.querySelector('.slider').addEventListener('click', ()=>{
-    console.log('slider triggered');
-    })
+const toggleButton = document.querySelector('.check-box');
+
+const prices = document.querySelectorAll('.price-monthly');
+
+
+
+
+function convertedPrice(priceElement, conversionFactor) {
+    const priceNumber = Number(priceElement.innerText);
+    const convertedPrice = (priceNumber * conversionFactor).toFixed(2);
+    priceElement.innerText = convertedPrice;
+}
+
+toggleButton.addEventListener("change", () => {
+    const conversionFactor = toggleButton.checked ? 0.1 : 10;
+    prices.forEach((price) => convertedPrice(price, conversionFactor));
+});
